fdd89d092f4cf19915902914e001dcaafcf68af8
Abhishek Khatod <abhishek.khatod@yash.com>
2019-01-09 11:30:12 +0530

    Updating java files : AK

--- src/xmlconfig/WriteXML.java
+++ src/xmlconfig/WriteXML.java
@@ -24,64 +24,75 @@ public class WriteXML {
 
 	static Properties prop;
 	static Document doc;
-	
+
 	public static void WriteXMLData(String FilePath, String FilePath_Prop, String FileName_XML, String FileName_Prop)
 			throws ParserConfigurationException, IOException, SAXException, TransformerException {
 
 		DocumentBuilderFactory Factory = DocumentBuilderFactory.newInstance();
 
 		DocumentBuilder Builder = Factory.newDocumentBuilder();
-		
+
 		File file = new File(FilePath + "\\" + FileName_XML);
 		doc = Builder.parse(file);
 
 		doc.getDocumentElement().normalize();
 
+		int count_add = 0, count_update_ele = 0, count_update_att = 0;
+
 		// To read data from properties file
 		prop = ReadXML.Read_Data_From_Properties(FilePath_Prop, FileName_Prop);
 
-		WriteXML.AddElement(FilePath, FilePath_Prop, FileName_XML, FileName_Prop);
+		count_add = WriteXML.AddElement(FilePath, FilePath_Prop, FileName_XML, FileName_Prop);
 
-		WriteXML.UpdateElementValue(FilePath, FilePath_Prop, FileName_XML, FileName_Prop);
-		
-		WriteXML.UpdateAttributeValue(FilePath, FilePath_Prop, FileName_XML, FileName_Prop);
-		
-		
-		//To append the updates in XML file
-		
+		count_update_ele = WriteXML.UpdateElementValue(FilePath, FilePath_Prop, FileName_XML, FileName_Prop);
+
+		count_update_att = WriteXML.UpdateAttributeValue(FilePath, FilePath_Prop, FileName_XML, FileName_Prop);
+
+		// To append the updates in XML file
+
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer = transformerFactory.newTransformer();
 		DOMSource source = new DOMSource(doc);
 		StreamResult result = new StreamResult(file);
 		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 		transformer.transform(source, result);
-		
-		System.out.println("XML File updated successfully..!!");
 
+		if (count_add > 0 && count_update_ele > 0 && count_update_att > 0) {
+
+			System.out.println("XML File updated successfully..!!");
+		} else {
+
+			System.out.println("Issue with one of the operations, please check as below :\n Add Operation :" + count_add
+					+ "\n Update Element :" + count_update_ele + "\n Update Attribute :" + count_update_att);
+		}
 	}
 
-	//For adding a new Element in the XML
-	
-	public static void AddElement(String FilePath, String FilePath_Prop, String FileName_XML,
-			String FileName_Prop) throws IOException {
+	// For adding a new Element in the XML
+
+	public static int AddElement(String FilePath, String FilePath_Prop, String FileName_XML, String FileName_Prop)
+			throws IOException {
 
 		prop = ReadXML.Read_Data_From_Properties(FilePath_Prop, FileName_Prop);
 
 		NodeList books = doc.getElementsByTagName(prop.getProperty("tag"));
 
 		Element book_1 = null;
+
+		int count = 0;
 		for (int i = 0; i < books.getLength(); i++) {
 
 			book_1 = (Element) books.item(i);
 			Element discount = doc.createElement(prop.getProperty("discount_tagname"));
 			discount.appendChild(doc.createTextNode(prop.getProperty("discount_value")));
 			book_1.appendChild(discount);
+			count++;
 		}
+		return count;
 	}
 
-	//For updating Element Value in the file
-	
-	public static void UpdateElementValue(String FilePath, String FilePath_Prop, String FileName_XML,
+	// For updating Element Value in the file
+
+	public static int UpdateElementValue(String FilePath, String FilePath_Prop, String FileName_XML,
 			String FileName_Prop) throws IOException {
 
 		prop = ReadXML.Read_Data_From_Properties(FilePath_Prop, FileName_Prop);
@@ -93,7 +104,7 @@ public class WriteXML {
 		String tag_name_value = prop.getProperty("tag_name_value");
 		String tag_value = prop.getProperty("tag_value");
 		String Attribute_Value = null;
-		
+		int count = 0;
 		// Access all the elements which are having "tag" as there value
 		for (int i = 0; i < books.getLength(); i++) {
 
@@ -101,40 +112,46 @@ public class WriteXML {
 
 			// Access the attribute value of specified "tag"
 			Attribute_Value = book_1.getAttribute(prop.getProperty("Attribute_Name"));
-			
+
 			Node Element_name = book_1.getElementsByTagName(tag_name).item(0).getFirstChild();
-			
-			if (Attribute_Value.equalsIgnoreCase(tag_value) && Element_name.getNodeValue().equalsIgnoreCase(tag_name_value)) {
-				
+
+			if (Attribute_Value.equalsIgnoreCase(tag_value)
+					&& Element_name.getNodeValue().equalsIgnoreCase(tag_name_value)) {
+
 				Element_name.setNodeValue("Abhishek");
+				count++;
 			}
 		}
+		return count;
 	}
 
 	// For Updating value of an attribute present in a tag
-	
-	public static void UpdateAttributeValue(String FilePath, String FilePath_Prop, String FileName_XML,
+
+	public static int UpdateAttributeValue(String FilePath, String FilePath_Prop, String FileName_XML,
 			String FileName_Prop) throws IOException {
 
 		prop = ReadXML.Read_Data_From_Properties(FilePath_Prop, FileName_Prop);
 
 		NodeList books = doc.getElementsByTagName(prop.getProperty("tag"));
-		
+
 		Element book_1 = null;
 		String Attribute_Value = null;
-		
-		for(int i=0; i<books.getLength();i++) {
-			
+		int count = 0;
+
+		for (int i = 0; i < books.getLength(); i++) {
+
 			book_1 = (Element) books.item(i);
 			Attribute_Value = book_1.getAttribute(prop.getProperty("Attribute_Name"));
-			
-			if(Attribute_Value.equalsIgnoreCase(prop.getProperty("tag_value"))) {
-				
+
+			if (Attribute_Value.equalsIgnoreCase(prop.getProperty("tag_value"))) {
+
 				book_1.setAttribute(prop.getProperty("Attribute_Name"), prop.getProperty("set_value"));
-			}else {
+				count++;
+			} else {
 				continue;
-			}	
-		}	
+			}
+		}
+		return count;
 	}
 
 }
